47bd904a505f6c34cfc16c2d54f1fb380550b863
Vinaykumar630 <140958914+Vinaykumar630@users.noreply.github.com>
2024-02-22 21:54:11 +0530

    Create index.js

---
+++ reactjs/TrackingForm/index.js
@@ -0,0 +1,57 @@
+import {Component} from 'react'
+import js-cookies from 'js-cookies
+
+class TackingForm extends Component {
+ state = {
+   trackingNumber: '',
+   errorMsg: '',
+   showError: false,
+  }
+  setTrackingNumber = event => {
+    this.setState({trackingNumber : event.target.value})
+  }
+   onSubmitSuccess = jwtToken => {
+    const {history} = this.props
+
+    Cookies.set('jwt_token', jwtToken, {
+      expires: 30,
+    })
+    history.replace('/')
+  }
+
+  onSubmitFailure = errorMsg => {
+    this.setState({showError: true, errorMsg})
+  }
+  handleTrack = () => {
+    const { trackingNumber} = this.state
+    const userDetails = {trackingNumber}
+    const url = 'https://apis.ccbp.in/login'
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+    const response = await fetch(url, options)
+    const data = await response.json()
+    if (response.ok === true) {
+      this.onSubmitSuccess(data.jwt_token)
+    } else {
+      this.onSubmitFailure(data.error_msg)
+    }
+  };
+  render() {
+    const {showError, trackingNumber, errorMsg} = this.state
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Enter Tracking Number"
+          value={trackingNumber}
+          onChange={this.setTrackNumber}
+        />
+        <button onClick={this.handleTrack}>Track</button>
+        {showError ? <p>{errorMsg}</p> : null }
+      </div>
+  );
+};
+
+export default TrackingForm;
